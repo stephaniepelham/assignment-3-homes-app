73cf603352d9dda8df71cc684dbf8051ba41d82e
Stephanie Pelham <stephaniepelham@dyn242-229.wireless-1725.ndsu.NoDak.edu>
2026-03-16 13:39:43 -0500

    Added routing to the angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import{Details } from './details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home Page' },
+  { path: 'details/:id', component: Details, title: 'Details Page' }
+];

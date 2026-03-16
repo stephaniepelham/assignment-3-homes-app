f1b1e084277024eb5d22acf669aab45b02b2979e
Stephanie Pelham <stephaniepelham@dyn242-229.wireless-1725.ndsu.NoDak.edu>
2026-03-16 15:51:09 -0500

    Added the search feature to the angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -15,8 +15,18 @@ export class Home{
 housingLocationList: Housinglocationinfo[] = [];
 // Injecting HousingService into the Component
 housingService: HousingService = inject(HousingService);
-constructor(){
-//setting housingLocationlist to the data array in HousingService
+filteredLocationList: Housinglocationinfo[] = [];
+constructor() {
 this.housingLocationList = this.housingService.getAllHousingLocations();
+this.filteredLocationList = this.housingLocationList;
+}
+filterResults(text: string) {
+if (!text) {
+this.filteredLocationList = this.housingLocationList;
+return;
+}
+this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+);
 }
 }
